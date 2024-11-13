1226f09fcb27e4e49d8a15c654f39e9b74461da2
David Greenland <David.Greenland@brighthr.com>
2024-11-13 11:06:24 +0000

    YLW-3252 (agifyController) changed namespace to file-scoped for consistency

--- DGWebApi/Controllers/AgifyController.cs
+++ DGWebApi/Controllers/AgifyController.cs
@@ -2,50 +2,47 @@
 using DGWebApi.Utils;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace DGWebApi.Controllers;
 
-namespace DGWebApi.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class AgifyController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AgifyController : ControllerBase
+    [HttpGet("{name}")]
+    public async Task<IActionResult> GetByName(string name, IAgifyService agifyService)
     {
-        [HttpGet("{name}")]
-        public async Task<IActionResult> GetByName(string name, IAgifyService agifyService)
-        {
-            var nameCount = await agifyService.GetName(name);
+        var nameCount = await agifyService.GetName(name);
 
-            if (nameCount == null)
-            {
-                return NotFound();
-            }
-            return Ok(nameCount);
+        if (nameCount == null)
+        {
+            return NotFound();
+        }
+        return Ok(nameCount);
+    }
+
+    [HttpGet("random")]
+    public async Task<IActionResult> Get(IAgifyService agifyService)
+    {
+        var nameCount = await agifyService.GetRandomName();
+
+        if (nameCount == null)
+        {
+            return NotFound();
+        }
+        return Ok(nameCount);
+    }
+
+    [HttpGet("{name}/{countryCode}")]
+    public async Task<IActionResult> GetByNameAndCountryCode(string name, string countryCode, IAgifyService agifyService)
+    {
+        var nameCount = await agifyService.GetByNameAndCountry(name, countryCode);
+
+        if (nameCount == null)
+        {
+            return NotFound();
         }
 
-        [HttpGet("random")]
-        public async Task<IActionResult> Get(IAgifyService agifyService)
-        {
-            var nameCount = await agifyService.GetRandomName();
-
-            if (nameCount == null)
-            {
-                return NotFound();
-            }
-            return Ok(nameCount);
-        }
-
-        [HttpGet("{name}/{countryCode}")]
-        public async Task<IActionResult> GetByNameAndCountryCode(string name, string countryCode, IAgifyService agifyService)
-        {
-            var nameCount = await agifyService.GetByNameAndCountry(name, countryCode);
-
-            if (nameCount == null)
-            {
-                return NotFound();
-            }
-
-            nameCount.CountryName = LocationUtils.GetNameFromCountryCode(countryCode);
-            return Ok(nameCount);
-        }
+        nameCount.CountryName = LocationUtils.GetNameFromCountryCode(countryCode);
+        return Ok(nameCount);
     }
 }
